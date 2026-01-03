9673a70a876a89d95d9506ed5629c8cb842872d3
emilioizaguirre <emilioizaguirre@uchicago.edu>
2026-01-03 15:08:40 -0600

    check for failure

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(void)
     ssize_t res = read(STDIN_FILENO, buf, 10);
     if (res == -1) return 1;
     s8 user_s = s8_buf(buf, 10);
+    if (user_s.len == 0) return -1;
     printf("this is a s8 string: \""p_s8"\" it has length %ld\n", S8_FMT(user_s), user_s.len);
 
     s8_free(user_s);
