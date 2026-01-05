1df4bc3b4c5e3b4cabdebfd73ddb05880f16e8ce
emilioizaguirre <emilioizaguirre@uchicago.edu>
2026-01-04 23:17:25 -0600

    added tests (debug) for strchr and strtok

--- main.c
+++ main.c
@@ -14,7 +14,25 @@ int main(void)
     s8 user_s = s8_buf(buf, 10);
     if (user_s.len == 0) return -1;
     printf("this is a s8 string: \""p_s8"\" it has length %ld\n", S8_FMT(user_s), user_s.len);
-
     s8_free(user_s);
+
+    s8 string_to_find_chars = S8_LIT("some chars 1234");
+    s8 find_s = s8_strchr(string_to_find_chars, 's');
+    s8_print_nl(find_s);
+    printf("%ld\n", find_s.len);
+    s8 find_1 = s8_strchr(string_to_find_chars, '1');
+    s8_print_nl(find_1);
+    printf("%ld\n", find_1.len);
+    s8 find_r = s8_strchr(string_to_find_chars, 'r');
+    s8_print_nl(find_r);
+    printf("%ld\n", find_r.len);
+    s8 find_p = s8_strchr(string_to_find_chars, 'p');
+    s8_print_nl(find_p);
+    printf("%ld\n", find_p.len);
+
+    s8 string_to_tokenize = S8_LIT("this is some long string");
+    for (s8 t = s8_strtok(string_to_tokenize, S8_LIT(" ")); t.len; t = s8_strtok(s8_null(), S8_LIT(" "))) {
+        s8_print_nl(t);
+    }
     return 0;
 }
